e090ce67b1504e0e2d8e5ff6d0d2bf1174e42617
Samuel Weslley M. Barboza <SWBarboza@raiadrogasil.com>
2020-04-08 00:43:39 -0300

    ta ficando legalzinho

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,13 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 export class NavbarComponent implements OnInit {
 
   links = [
-    { title: 'Dashboard', fragment: '/dashboard', img: '../../../assets/img/dashboard.png' },
-    { title: 'Produtos', fragment: '/produtos', img: '../../../assets/img/trolley.png' },
-    { title: 'Clientes', fragment: '/clientes', img: '../../../assets/img/person.png'  },
-    { title: 'Pedidos', fragment: '/pedidos', img: '../../../assets/img/box.png'  }
+    { id:1, title: 'Dashboard', fragment: '/dashboard', imgCheck: '../../../assets/img/dashboard_check.png', img: '../../../assets/img/dashboard.png', selected:false},
+    { id:2, title: 'Produtos', fragment: '/produtos', imgCheck:"../../../assets/img/inventory_check.png", img: '../../../assets/img/trolley.png', selected:false },
+    { id:3, title: 'Clientes', fragment: '/clientes', imgCheck: '../../../assets/img/person_check.png', img: '../../../assets/img/person.png', selected:false },
+    { id:4, title: 'Pedidos', fragment: '/pedidos', imgCheck: '../../../assets/img/box_check.png', img: '../../../assets/img/box.png', selected:false }
   ]
 
   constructor(public route: ActivatedRoute) {  }
+
+  isCheck(id: number) {
+    for(let i = 0; i <= this.links.length; i++) {
+      if(this.links[i].id == id) {
+        this.links[i].selected = true;
+      } else {
+        this.links[i].selected = false;
+      }
+    }
+  }
 
   ngOnInit(): void {
   }
